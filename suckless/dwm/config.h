2752844bb7e189a8354c5cbf6a82dd6bd1b40dcd
bocho8 <bochobocho3000@gmail.com>
2024-05-11 20:53:53 -0300

    Aded icons to tags

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -29,7 +29,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", " ", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
